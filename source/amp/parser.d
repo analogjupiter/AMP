f1807c8061deff9e777c110bcdf26edeca1facac
0xEAB <desisma@heidel.beer>
2018-05-17 17:41:33 +0200

    Add linking exception to license
    Based on the GNU Classpath license

--- source/amp/parser.d
+++ source/amp/parser.d
@@ -15,6 +15,23 @@
 
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Linking this tool statically or dynamically with other modules is
+    making a combined work based on this tool.  Thus, the terms and
+    conditions of the GNU Affero General Public License cover the whole
+    combination.
+
+    As a special exception, the copyright holders of this tool give you
+    permission to link this tool with independent modules to produce an
+    executable, regardless of the license terms of these independent
+    modules, and to copy and distribute the resulting executable under
+    terms of your choice, provided that you also meet, for each linked
+    independent module, the terms and conditions of the license of that
+    module.  An independent module is a module which is not derived from
+    or based on this tool.  If you modify this tool, you may extend
+    this exception to your version of the tool, but you are not
+    obligated to do so.  If you do not wish to do so, delete this
+    exception statement from your version.
  +/
 module amp.parser;
 
@@ -143,3 +160,4 @@ ParserResult parseBlueprint(string filePath)
     writeln(api.to!(string));
     return r;
 }
+
